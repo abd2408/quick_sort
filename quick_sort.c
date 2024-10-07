dcb868c90ed3dd19e532943d297ebf7fbc629d0c
Sizning Ismingiz <you@example.com>
2024-10-07 22:08:53 +0500

    Va nihoyat o'rgandim

--- quick_sort.c
+++ quick_sort.c
@@ -1,51 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 void print(int arr[], int l)
 {
-    for (int i = 0; i < l; i++)
+    for (int i = 0; i <= l; i++)
     {
         printf("%d\t", arr[i]);
     }
-} 
+    printf("\n");
+}
 
-void q(int arr[], int i, int l){
-    int a = arr[l]; // a = 0
-    for (int k = i - 1; i <= l; i++){
-        if (arr[i] <= a){
-            if (arr[i] == a && i != l){
-                k--;
-            }
-            k++;
-            if (k == i){
-                continue;
-            }
-            else if (k < i){
-                int swap = arr[i];
-                arr[i] = arr[k];
-                arr[k] = swap;
-            }
-        }
-    }
-    for (int i = 0; i <= l; i++){
-        if (arr[i] == a && i != 0 && i != l){
-            q(arr, 0, i - 1);
-            q(arr, i + 1, l);
-        }
-        else if (i == l && arr[i] == a){
-            q(arr, 0, l - 1);
-        }
-        else if (i == 0 && arr[i] == a){
-            q(arr, 1, l);
-        }
+void fill(int arr[], int l)
+{
+    for (int i = 0; i < l; i++)
+    {
+        arr[i] = rand() % 100;
     }
 }
 
-int main() {
-    system("cls");
-    
-    int arr[] = {3,7,4,9,0,1,5,2,8};
-    int l = sizeof(arr) / sizeof(arr[0]);
-    q(arr, 0, l-1);
-    print(arr, l);
+int pivot_index(int arr[], int min, int l)
+{
+    int k = -1;
+    int a = arr[l];
+    for (int i = min; i < l; i++)
+    {
+        if (arr[i] <= a)
+        {
+            k++;
+            int t = arr[i];
+            arr[i] = arr[k];
+            arr[k] = t;
+        }
+    }
+    int t = arr[k + 1];
+    arr[k + 1] = arr[l];
+    arr[l] = t;
+    return k + 1;
+}
+void sort(int arr[], int min, int pi)
+{
+    if (min < pi)
+    {
+        int pivot_i = pivot_index(arr, 0, pi);
+        sort(arr, min, pivot_i - 1);
+        sort(arr, pivot_i + 1, pi);
+    }
+}
+
+int main()
+{
+    // system("cls");
+    srand(time(NULL));
+    int l;
+    printf("Arrayda nechta son bo'lsin: ");
+    scanf("%d", &l);
+    int arr[l];
+    fill(arr, l);
+    print(arr, l - 1);
+    sort(arr, 0, l - 1);
+    print(arr, l - 1);
+    return 0;
 }
